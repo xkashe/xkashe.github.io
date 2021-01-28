440f89f5f24a7cef60341857591b8f12ae50cde4
xkashe <68208139+xkashe@users.noreply.github.com>
2021-01-28 22:24:35 +0100

    Update GlobalStyles.jsx

--- src/components/GlobalStyles.jsx
+++ src/components/GlobalStyles.jsx
@@ -11,7 +11,7 @@ export default createGlobalStyle`
     text-align: center;
     font-size: 20px;
     color: #f1f1f1;
-    background-color: #333333;
+    background-color: #33333;
     font-weight: 500;
   }
   h1, h2, h3, h4, h5, h6 {
