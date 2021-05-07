d1b0c8724247a284a01f672f86bd844dc1e20268
xkashe <68208139+xkashe@users.noreply.github.com>
2021-05-07 22:31:05 +0200

    Update VFX.jsx

--- src/sections/VFX.jsx
+++ src/sections/VFX.jsx
@@ -5,24 +5,9 @@ import ItemGrid from 'components/ItemGrid';
 import {Video} from "components/Video";
 
 const VIDEOS = [
-  'twyKbPS72IQ',
+  'CNM8WDaJGGS',
   '9K2JwBQSm14',
-  'pWck9174UqE',
-  'cYJUPdjhAdE',
-  'mp7ftGRfxqY',
-  'FtKRKuzIkis',
-  'uEkFVJJfm7Y',
-  'BINDDd5UJMk',
-  '3nowEnpg14g',
-  'KBy3vrKgh_0',
-  '6dBd-ZqT2H0',
-  'HsbJJAIYglA',
-  'uQrM58B671E',
-  'zjJJkKP21bg',
-  'p1ZUMPllya4',
-  '8TDlN0ZB6_I',
-  'rqaxqnE9KGI',
-  'rzfM06oyB1U',
+ 
 ];
 
 const VFX = () => (
